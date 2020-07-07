c54a82473fcac816a4f173da1dee9f57067aefd4
dileeps861 <dileeps861@hotmail.com>
2020-07-07 15:51:56 +0530

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -5,23 +5,26 @@ import edu.princeton.cs.algs4.StdRandom;
 import java.util.Iterator;
 
 public class Permutation {
+    
     public static void main(String[] args) {
+        //Permutation with improved memory. This algorithm usage only K size of RandomizedQueue.
+        
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> s = new RandomizedQueue<>();
         int n = 0;
         while (!StdIn.isEmpty()) {
-            n++;
-            if (k <= 0) StdIn.readString();
+            n++;    // Need to increase n before taking random to improve probablity
+            if (k <= 0) StdIn.readString(); //  In case of k<0 do nothing but read string from StdIn
             else if (n <= k) {
-                s.enqueue(StdIn.readString());
+                s.enqueue(StdIn.readString());  //  In case of count of read string from StdIn are less than k, no need to opt out the string
             }
             else {
-                if (StdRandom.uniform(n) < k) {
+                if (StdRandom.uniform(n) < k) {  // Take random uniformly in [0, n).
                     s.dequeue();
                     s.enqueue(StdIn.readString());
                 }
                 else {
-                    StdIn.readString();
+                    StdIn.readString();     //  Skip the String if the RandomizedQueue is already full;
                 }
 
             }
